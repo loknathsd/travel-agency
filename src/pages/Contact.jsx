626c333e2fbcd368e80c6d724cf789cd78e3aac2
loknathsd <loknathsds66@gmail.com>
2023-04-08 20:41:00 +0600

    contact updated

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,8 +7,8 @@ const Contact = () => {
     return (
         <div className='w-[80%] mx-auto mt-24'>
             <h1 className='text-4xl font-serif mb-5'>Get in Touch</h1>
-            <div className='lg:flex justify-between '>
-                <div className='max-sm:mb-12'>
+            <div className='lg:flex justify-between'>
+                <div className='max-sm:mb-12 lg:w-[60%]'>
                     <form action="">
                         <textarea placeholder='Your Message' className='bg-transparent border border-[#e5e6e9] w-full px-5 py-5' ></textarea> <br />
                         <div className='flex mt-5'>
@@ -19,23 +19,23 @@ const Contact = () => {
                         <input className='px-12 py-2 rounded mt-5 bg-red-400 text-white ' type="submit" value="Send" />
                     </form>
                 </div>
-                <div className=' font-serif'>
+                <div className=' font-serif '>
                     <div className='flex'>
-                        <AiOutlineHome className='text-4xl mr-5'  />
+                        <AiOutlineHome className='text-4xl mr-5 mt-2'  />
                         <div>
                         <h1 className='text-xl '>Buttonwood, California.</h1>
                         <p className='text-gray-500'>Rosemead, CA 91770</p>
                         </div>
                     </div>
                     <div className='flex my-5'>
-                        <FaMobileAlt className='text-4xl mr-5'  />
+                        <FaMobileAlt className='text-4xl mr-5 mt-2'  />
                         <div>
                         <h1 className='text-xl '>+1 253 565 2365 </h1>
                         <p className='text-gray-500'>Mon to Fri 9am to 6pm</p>
                         </div>
                     </div>
                     <div className='flex'>
-                       <AiOutlineMail  className='text-4xl mr-5' />
+                       <AiOutlineMail  className='text-4xl mr-5 mt-2' />
                        <div>
                        <h1 className='text-xl '>support@colorlib.com</h1>
                         <p className='text-gray-500'>Send us your query anytime!</p>
